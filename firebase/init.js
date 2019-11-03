2d34a92b832d71f6f2536448d905ec300f8992e9
adjeteyboy <adjeieric4@yahoo.com>
2019-11-03 23:16:44 +0000

    left with applyforcare page

--- firebase/init.js
+++ firebase/init.js
@@ -1,9 +1,7 @@
-const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyD-xxLYiX4tkyPfaUCeNrnQkrNLFOHENsU",
-  authDomain: "barnes-angels.firebaseapp.com",
   databaseURL: "https://barnes-angels.firebaseio.com",
   projectId: "barnes-angels",
-  storageBucket: "",
   messagingSenderId: "217696851131",
   appId: "1:217696851131:web:049d73361c3d961fb0459e"
 };
